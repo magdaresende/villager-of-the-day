ba79d1f967432272cc97b1c664d0e7fffb0e12fa
Magda Resende <magda.ca.resende@gmail.com>
2021-05-25 11:29:33 +0100

    Add villagers file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import styles from './styles/App.module.scss';
 import Card from './Card.js';
 
-const villager = require('./villager.json');
+const villagers = require('./villagers.json');
 
 export default function App() {
+  const villagerID = getRandomIntInclusive();
+  const villagerCode = getKeyByValue(villagers, villagerID);
+  const villager = villagers[villagerCode];
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.title}>Villager of the day</div>
+      <div className={styles.title}>Who is this villager?</div>
       <Card villager={villager} />
     </div>
   );
+}
+
+function getRandomIntInclusive() {
+  const min = 1;
+  const max = 391;
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getKeyByValue(object, value) {
+  return Object.keys(object).find((key) => object[key].id === value);
 }
